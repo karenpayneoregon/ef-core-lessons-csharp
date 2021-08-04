34c5c50bb03449a013ea2f78aaf6d572d0dafb66
Karen Payne <Karen.1.Payne@oregon.gov>
2021-08-04 02:50:01 -0700

    In memory testing

---
+++ NorthWindCoreUnitTest_InMemory/MainTest.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NorthWindCoreUnitTest_InMemory.Base;
+
+namespace NorthWindCoreUnitTest_InMemory
+{
+    [TestClass]
+    public partial class MainTest : TestBase
+    {
+        [TestMethod]
+        [TestTraits(Trait.PlaceHolder)]
+        public void TestMethod1()
+        {
+            // arrange
+
+
+            // act
+
+
+            // assert
+        }
+        [TestMethod]
+        [TestTraits(Trait.PlaceHolder)]
+        public void TestMethod2()
+        {
+            // TODO
+        }
+    }
+}
